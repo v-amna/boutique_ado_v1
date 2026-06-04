beb3cd431ef5cf3880e534252cfbb72a27701b31
v-amna <amnamampad@gmail.com>
2026-06-04 13:26:19 +0200

    Added forms ,admin and signals

---
+++ checkout/signals.py
@@ -0,0 +1,21 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import OrderLineItem
+
+@receiver(post_save, sender=OrderLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Update order total on lineitem update/create
+    """
+    instance.order.update_total()
+
+
+@receiver(post_delete, sender=OrderLineItem)
+def update_on_save(sender, instance, **kwargs):
+    # chris made a mistake here, this function should have 
+    # been called update_on_delete. He fixes this in an upcoming video.
+    """
+    Update order total on lineitem delete
+    """
+    instance.order.update_total()
