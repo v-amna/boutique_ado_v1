457bec258bccf803e9585a595e0d07a67792eecd
v-amna <amnamampad@gmail.com>
2026-06-08 18:38:11 +0200

    Add Stripe integration for payment processing with styling and scripts

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51TeumHIkXOU5Pgli1nTQaUkh3NyaCDEGcfCuc6MbEotjg0uH5VDdxsq9Ur28rQo5lfMBYruOaiUqeUz5rkQlKKHn00BDOlTyAn',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
